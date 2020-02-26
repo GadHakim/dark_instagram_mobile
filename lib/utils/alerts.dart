b2d3b07a7a3ece3b63daf64ab58088714df1c3d5
Vlad Kozlov <vlad.kozlov@engineering-idea.com>
2020-02-26 20:41:15 +0200

    feat(alert): added message alert dialog

---
+++ lib/utils/alerts.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Future<void> showDialogMessage(
+  BuildContext context,
+  String title,
+  String message,
+) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}

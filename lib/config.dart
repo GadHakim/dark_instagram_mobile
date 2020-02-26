808b26bac75b44c80b7686cc036f80dac1ff992b
Vlad Kozlov <vlad.kozlov@engineering-idea.com>
2020-02-26 20:44:01 +0200

    feat(config): added internet config

---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+class Internet {
+  static const String BASE_URL = 'http://192.168.0.100:3000';
+  static const String AUTH = BASE_URL + '/auth';
+  static const String SIGN_IN = AUTH + '/login';
+}

8b229630480f16046b956f55459e0ec9c19b7602
Vlad Kozlov <vlad.kozlov@engineering-idea.com>
2020-02-27 11:03:26 +0200

    feat(auth_repository): added sign up method

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,10 +1,18 @@
 import 'package:http/http.dart' as http;
 import 'package:instagram/config.dart';
 import 'package:instagram/data/models/sign_in_model.dart';
+import 'package:instagram/data/models/sign_up_model.dart';
 import 'package:instagram/utils/http.dart';
 
 abstract class AuthRepository {
   Future<SignInModel> signIn(
+    String email,
+    String password,
+  );
+
+  Future<SignUpModel> signUp(
+    String firstName,
+    String lastName,
     String email,
     String password,
   );
@@ -24,5 +32,24 @@ class AuthRepositoryImpl extends AuthRepository {
     return request(response, (data) {
       return SignInModel.fromJson(data);
     });
+  }
+
+  @override
+  Future<SignUpModel> signUp(
+    String firstName,
+    String lastName,
+    String email,
+    String password,
+  ) async {
+    var response = await http.post(Internet.SIGN_UP, body: {
+      "first_name": firstName,
+      "last_name": lastName,
+      "email": email,
+      "password": password,
+    });
+
+    return request(response, (data) {
+      return SignUpModel.fromJson(data);
+    }, statusCodeSuccess: 201);
   }
 }

95906d3172db77311b2e6234d0f61ad4d94da580
Vlad Kozlov <vlad.kozlov@engineering-idea.com>
2020-02-29 18:21:08 +0200

    feat(post_creation): created post creation page

---
+++ lib/ui/pages/post_creation_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PostCreationPage extends StatefulWidget {
+  @override
+  _PostCreationPageState createState() => _PostCreationPageState();
+}
+
+class _PostCreationPageState extends State<PostCreationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Post Creation Page'),
+      ),
+    );
+  }
+}

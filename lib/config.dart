9e42c919f827f9f9a6edc1bd5c59dad74a29ff13
Vlad Kozlov <vlad.kozlov@engineering-idea.com>
2020-03-01 19:53:16 +0200

    feat(subscribers_repository): added repository

--- lib/config.dart
+++ lib/config.dart
@@ -2,7 +2,7 @@ class Endpoint {
   static const String BASE_URL = '192.168.0.100:3000';
 
   //Auth
-  static const String AUTH = 'auth';
+  static const String AUTH = 'auth/';
   static const String LOGIN = '$AUTH/login';
   static const String REGISTRATION = '$AUTH/registration';
 
@@ -11,8 +11,13 @@ class Endpoint {
   static const String GET_SUBSCRIBERS = 'people/subscribers';
 
   //Post
-  static const String POST = 'publication';
+  static const String POST = 'publication/';
   static const String ADD_POST = '$POST/';
   static const String ALL_POST = '$POST/all';
   static const String SUBSCRIBERS_POSTS = '$POST/subscribers';
+
+  //Subscribers
+  static const String SUBSCRIBERS = 'subscribers/';
+  static const String SUBSCRIBE = '$SUBSCRIBERS/subscribe';
+  static const String UNSUBSCRIBE = '$SUBSCRIBERS/unsubscribe';
 }

dc85311615fb08d5747b706395cd926bfe8488e5
Vlad Kozlov <vlad.kozlov@engineering-idea.com>
2020-03-01 09:30:16 +0200

    feat(post): added request add post

--- lib/config.dart
+++ lib/config.dart
@@ -11,6 +11,7 @@ class Endpoint {
 
   //Post
   static const String POST = 'publication';
+  static const String ADD_POST = '$POST/';
   static const String ALL_POST = '$POST/all';
   static const String SUBSCRIBERS_POSTS = '$POST/subscribers';
 }
--- lib/data/repositories/post_repository.dart
+++ lib/data/repositories/post_repository.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
+import 'package:async/async.dart';
 import 'package:http/http.dart';
 import 'package:instagram/config.dart';
 import 'package:instagram/data/models/all_post_model.dart';
 import 'package:instagram/data/models/subscribers_posts_model.dart';
+import 'package:instagram/data/store.dart';
 import 'package:instagram/utils/http.dart';
+import 'package:path/path.dart';
 
 abstract class PostRepository {
   Future<AllPostModel> getAllPost({
@@ -12,10 +17,16 @@ abstract class PostRepository {
   Future<SubscribersPostsModel> getSubscribersPosts({
     int limit,
   });
+
+  Future addPost({
+    String comment,
+    List<File> file,
+  });
 }
 
 class PostRepositoryImpl extends PostRepository {
   final Http http;
+  final Store store = Store();
 
   PostRepositoryImpl(this.http);
 
@@ -43,5 +54,37 @@ class PostRepositoryImpl extends PostRepository {
     return Http.readResponse(response, (data) {
       return SubscribersPostsModel.fromJson(data);
     });
+  }
+
+  @override
+  Future addPost({
+    String comment,
+    List<File> file,
+  }) async {
+    print(file);
+
+    var uri = Uri.http(Endpoint.BASE_URL, Endpoint.ADD_POST);
+    var request = new MultipartRequest("POST", uri);
+    request.headers['Authorization'] = "Bearer ${store.accessToken}";
+    request.fields['comment'] = comment;
+
+    List<MultipartFile> newList = new List<MultipartFile>();
+    for (int i = 0; i < file.length; i++) {
+      File imageFile = file[i];
+      var stream = new ByteStream(DelegatingStream.typed(imageFile.openRead()));
+      var length = await imageFile.length();
+      var multipartFile = new MultipartFile(
+        "content",
+        stream,
+        length,
+        filename: basename(imageFile.path),
+      );
+      newList.add(multipartFile);
+    }
+    request.files.addAll(newList);
+    var response = await request.send();
+    if (response.statusCode != 200) {
+      throw Exception();
+    }
   }
 }
